7d96225db53e4ba0086001cf72da55bb86a2084f
AllenVillalobos <allenvs06@gmail.com>
2025-12-10 11:23:18 -0600

    Agregar archivos de proyecto.

---
+++ Contexto/ContextoBaseDatos.cs
@@ -0,0 +1,19 @@
+﻿using APIExamen.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExamen.Contexto
+{
+    public class ContextoBaseDatos : DbContext
+    {
+        public ContextoBaseDatos(DbContextOptions<ContextoBaseDatos> options) : base(options)
+        {
+
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiante>().HasKey(e => e.EstudianteID);
+        }
+    }
+}
---
+++ Modelos/Estudiante.cs
@@ -0,0 +1,27 @@
+﻿namespace APIExamen.Modelos
+{
+    public class Estudiante
+    {
+        public int EstudianteID { get; set; }
+
+        public string? Identificacion { get; set; }
+
+        public string? PrimerNombre { get; set; }
+
+        public string? PrimerApellido { get; set; }
+
+        public string? SegundoNombre { get; set; }
+
+        public string? SegundoApellido { get; set; }
+
+        public DateTime? FechaNacimiento { get; set; }
+
+        public int? Edad { get; set; }
+
+        public string? Direccion { get; set; }
+
+        public DateTime? FechaCreacion { get; set; }
+
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
